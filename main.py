ec5ca00fae260da6d45b67fa11230f74aa8d7efd
mahnaztalebi <maryamt132007@gmail.com>
2024-10-22 23:25:27 -0400

    Maryam added decoder function to main.py

--- main.py
+++ main.py
@@ -18,8 +18,17 @@ def encoder(password):
         encoded_password += str(digits + 3)
 
     return encoded_password
+    
+def decode(encoded_password):
+    decoded_pass = ''
+    for digits in encoded_password:
+        decoded_pass += str(int(digits) - 3)
+    return decoded_pass
+        
+    
 
 def main():
+    encoded_password = ''
     while True:
         #menu
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
@@ -27,15 +36,15 @@ def main():
 
         if menu_option == 1:
             password = input("Please enter your password to encode: ")
-            encoder(int(password))
+            encoded_password = encoder(int(password))
             print("Your password has been encoded and stored!\n")
 
-        if menu_option == 2:
-            pass
+        elif menu_option == 2:
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
 
-        if menu_option == 3:
+        elif menu_option == 3:
             break
         
-
 if __name__ == "__main__":
     main()
